072f9c02cbb1e3ae8b53f657b745b7e8d9e7dcda
Jack Kavanagh <jackkav@gmail.com>
2019-03-11 22:57:04 +0100

    Tidy css

--- src/App.vue
+++ src/App.vue
@@ -24,38 +24,4 @@ body,
   flex-direction: column;
 }
 
-.main {
-  background: rgb(88, 124, 88);
-  display: flex;
-  flex: 1;
-  /* mind this */
-  /*overflow:auto; */ /* can come handy here if you want to keep footer in view */
-}
-
-.nav {
-  background: rgb(145, 101, 101);
-  flex: 0 0 12em;
-}
-
-.content {
-  background: gray;
-  flex: 1 0 auto;
-  display: flex;
-  flex-direction: column;
-}
-
-.row {
-  display: flex;
-  flex: 1 0 auto;
-  flex-direction: row;
-}
-
-.col {
-  flex: 1 0 auto;
-}
-/* 
-* {
-  box-shadow: inset 0px 0px 0px 1px #f00;
-} */
-
 </style>
