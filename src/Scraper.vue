3eca9c75b21a92c8704fd8accca9155eb2b602a8
Jack Kavanagh <jackkav@gmail.com>
2019-06-17 14:33:56 +0200

    refactor to pug

--- src/Scraper.vue
+++ src/Scraper.vue
@@ -1,25 +1,36 @@
-<template>
-  <div>
-    <div v-for="d in items.filter(x=>x.hd)" :key="d.id">
-      <div
-        v-on:click="d.show = !d.show"
-        class="ba ph3 pv3 bg-washed-green black bg-animate hover-bg-dark-green hover-white"
-      >{{d.movieTitle}} [{{d.uploadedAtFromNow}}]</div>
-      <transition name="fade">
-        <div
-          v-if="d.show"
-          class="ph3 pv3 bg-washed-green black bg-animate hover-bg-dark-green hover-white"
-        >
-          <div>Name: {{d.title}}</div>
-          <div>Position: #{{d.index + 1}}</div>
-          <div>Quality: {{d.quality}}</div>
-          <div>Size: {{d.size}}</div>
-          <a class="f6 link dim br2 ph3 pv2 mb2 dib white bg-black" v-bind:href="d.magnet">Download</a>
-          <trailer :title="d.movieTitle" :year="d.year"/>
-        </div>
-      </transition>
-    </div>
-  </div>
+<template lang="pug">
+div
+  div(v-for="d in items.filter(x=>x.hd)" :key="d.id")
+    .ba.ph3.pv3.bg-washed-green.black.bg-animate.hover-bg-dark-green.hover-white(@click="d.show = !d.show") {{d.movieTitle}} [{{d.uploadedAtFromNow}}]
+    transition(name="fade")
+      .ph3.pv3.bg-washed-green.black.bg-animate.hover-bg-dark-green.hover-white(v-if="d.show")
+        div Name: {{d.title}}
+        div Position: # {{d.index + 1}}
+        div Quality: {{d.quality}}
+        div Size: {{d.size}}
+        .f6.link.dim.br2.ph3.pv2.mb2.dib.white.bg-black(:href="d.magnet") Download
+        trailer(:title="d.movieTitle" :year="d.year")
+  //- <div>
+  //-   <div v-for="d in items.filter(x=>x.hd)" :key="d.id">
+  //-     <div
+  //-       v-on:click="d.show = !d.show"
+  //-       class="ba ph3 pv3 bg-washed-green black bg-animate hover-bg-dark-green hover-white"
+  //-     >{{d.movieTitle}} [{{d.uploadedAtFromNow}}]</div>
+  //-     <transition name="fade">
+  //-       <div
+  //-         v-if="d.show"
+  //-         class="ph3 pv3 bg-washed-green black bg-animate hover-bg-dark-green hover-white"
+  //-       >
+  //-         <div>Name: {{d.title}}</div>
+  //-         <div>Position: #{{d.index + 1}}</div>
+  //-         <div>Quality: {{d.quality}}</div>
+  //-         <div>Size: {{d.size}}</div>
+  //-         <a class="f6 link dim br2 ph3 pv2 mb2 dib white bg-black" v-bind:href="d.magnet">Download</a>
+  //-         <trailer :title="d.movieTitle" :year="d.year"/>
+  //-       </div>
+  //-     </transition>
+  //-   </div>
+  //- </div>
 </template>
 
 <script>
