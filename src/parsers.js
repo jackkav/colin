be72968a47775841328d7e813b89147df20bfcd4
Jack Kavanagh <jackkav@gmail.com>
2019-06-17 14:35:03 +0200

    Fix tc matcher

--- src/parsers.js
+++ src/parsers.js
@@ -1,46 +1,50 @@
 export const sanitiseMusic = input =>
   input
-    .replace(/\(\d{4}\)/g, '')
-    .replace(/mp3/gi, '')
-    .replace(/\(320kbps\)/g, '')
-    .replace(/320kbps/g, '')
-    .replace(/\[.*\]/g, '')
-    .trim()
+    .replace(/\(\d{4}\)/g, "")
+    .replace(/mp3/gi, "")
+    .replace(/\(320kbps\)/g, "")
+    .replace(/320kbps/g, "")
+    .replace(/\[.*\]/g, "")
+    .trim();
 const getUploadMetadata = input => {
   const size =
     input.match(/Size (.*?), ULed/) &&
     input.match(/Size (.*?), ULed/).length > 1 &&
     input
       .match(/Size (.*?), ULed/)[1]
-      .replace('MiB', 'MB')
-      .replace('GiB', 'GB')
+      .replace("MiB", "MB")
+      .replace("GiB", "GB");
 
-  const uploadedIndex = input.indexOf('Uploaded')
-  const sizeIndex = input.indexOf(' Size')
-  const endOfTitleIndex = uploadedIndex
+  const uploadedIndex = input.indexOf("Uploaded");
+  const sizeIndex = input.indexOf(" Size");
+  const endOfTitleIndex = uploadedIndex;
   const y =
-    input.match(/2015/) || input.match(/2016/) || input.match(/2017/) || input.match(/2018/) || input.match(/2019/)
-  const year = y ? y.toString() : new Date().getFullYear().toString()
-  const yearIndex = input.slice(0, endOfTitleIndex - 1).indexOf(year)
+    input.match(/2015/) ||
+    input.match(/2016/) ||
+    input.match(/2017/) ||
+    input.match(/2018/) ||
+    input.match(/2019/);
+  const year = y ? y.toString() : new Date().getFullYear().toString();
+  const yearIndex = input.slice(0, endOfTitleIndex - 1).indexOf(year);
   const title = input
     .slice(0, endOfTitleIndex - 1)
-    .replace(/\./g, ' ')
-    .replace(/\n/g, '')
-    .replace(/\t/g, '')
-    .trim()
+    .replace(/\./g, " ")
+    .replace(/\n/g, "")
+    .replace(/\t/g, "")
+    .trim();
   const uploadedAtTime = input
     .slice(uploadedIndex + 9, sizeIndex - 1)
-    .replace(/\./g, ' ')
-    .replace(/\n/g, '')
-    .replace(/\t/g, '')
-  const uploadedAt = parseLooseDate(uploadedAtTime)
+    .replace(/\./g, " ")
+    .replace(/\n/g, "")
+    .replace(/\t/g, "");
+  const uploadedAt = parseLooseDate(uploadedAtTime);
   const full = input
-    .replace(/\./g, ' ')
-    .replace(/\n/g, '')
-    .replace(/\t/g, '')
-    .trim()
-  return { title, yearIndex, size, year, uploadedAt, full }
-}
+    .replace(/\./g, " ")
+    .replace(/\n/g, "")
+    .replace(/\t/g, "")
+    .trim();
+  return { title, yearIndex, size, year, uploadedAt, full };
+};
 const getMovieMetadata = (input, yearIndex) => {
   const r =
     input.match(/HDRip/i) ||
@@ -57,7 +61,8 @@ const getMovieMetadata = (input, yearIndex) => {
     input.match(/HDTS/i) ||
     input.match(/HD TS/i) ||
     input.match(/TS/i) ||
-    input.match(/HD TC/i)
+    input.match(/HDTC/i) ||
+    input.match(/HD TC/i);
   const lowQuality =
     input.match(/CAM/i) ||
     input.match(/HDCAM/i) ||
@@ -68,47 +73,54 @@ const getMovieMetadata = (input, yearIndex) => {
     input.match(/HDTS/i) ||
     input.match(/HD TS/i) ||
     input.match(/TS/i) ||
-    input.match(/HD TC/i)
+    input.match(/HDTC/i) ||
+    input.match(/HD TC/i);
 
-  const quality = r ? r.toString() : 'N/A'
-  const hd = !lowQuality
-  const qualityIndex = input.indexOf(quality)
+  const quality = r ? r.toString() : "N/A";
+  const hd = !lowQuality;
+  const qualityIndex = input.indexOf(quality);
 
-  const endOfMovieTitle = yearIndex > 0 ? yearIndex : qualityIndex
+  const endOfMovieTitle = yearIndex > 0 ? yearIndex : qualityIndex;
   const movieTitle = input
     .slice(0, endOfMovieTitle - 1)
-    .replace(/\./g, ' ')
-    .replace(/\n/g, '')
-    .replace(/\t/g, '')
-    .trim()
-  return { hd, movieTitle, quality }
-}
+    .replace(/\./g, " ")
+    .replace(/\n/g, "")
+    .replace(/\t/g, "")
+    .trim();
+  return { hd, movieTitle, quality };
+};
 
 const topics = {
-  movies: 'aggro.pb.201',
-  music: 'aggro.pb.101',
-  tv: 'aggro.pb.205',
-}
+  movies: "aggro.pb.201",
+  music: "aggro.pb.101",
+  tv: "aggro.pb.205"
+};
 export const pbParse = (input, type = topics.movies) => {
-  if (!input) return false
-  const meta = getUploadMetadata(input)
+  if (!input) return false;
+  const meta = getUploadMetadata(input);
   if (type === topics.music) {
-    return meta
+    return meta;
   }
   if (type === topics.movies) {
     return {
       ...meta,
-      ...getMovieMetadata(input, meta.yearIndex),
-    }
+      ...getMovieMetadata(input, meta.yearIndex)
+    };
   }
-}
+};
 
 const parseLooseDate = uploadedAt => {
   // console.log(uploadedAt)
-  const today = new Date()
-  if (uploadedAt.includes('Today')) return new Date().toISOString()
-  else if (uploadedAt.includes('Y-day')) return new Date(today.setDate(today.getDate() - 1)).toISOString()
-  else if (uploadedAt.includes(':'))
-    return new Date(today.getUTCFullYear() + '-' + uploadedAt.replace(' ', 'T')).toISOString()
-  else return new Date(uploadedAt.split(' ')[1] + '-' + uploadedAt.split(' ')[0]).toISOString()
-}
+  const today = new Date();
+  if (uploadedAt.includes("Today")) return new Date().toISOString();
+  else if (uploadedAt.includes("Y-day"))
+    return new Date(today.setDate(today.getDate() - 1)).toISOString();
+  else if (uploadedAt.includes(":"))
+    return new Date(
+      today.getUTCFullYear() + "-" + uploadedAt.replace(" ", "T")
+    ).toISOString();
+  else
+    return new Date(
+      uploadedAt.split(" ")[1] + "-" + uploadedAt.split(" ")[0]
+    ).toISOString();
+};
